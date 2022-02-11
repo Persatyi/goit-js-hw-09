3dd7d7fbac090b9227a0243be59f410dee8b9a07
Oleksii Persatyi <persatyi@ukr.net>
2022-02-11 22:02:54 +0200

    задание 2 и 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,11 +1,17 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const refs = {
   startBtn: document.querySelector('[data-start]'),
+  dataDays: document.querySelector('[data-days]'),
+  dataHours: document.querySelector('[data-hours]'),
+  dataMinutes: document.querySelector('[data-minutes]'),
+  dataSeconds: document.querySelector('[data-seconds]'),
 };
 
 let chosenDate = null;
+let intervalId = null;
 
 const options = {
   enableTime: true,
@@ -13,10 +19,15 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    chosenDate = selectedDates[0].getTime();
+    chosenDate = selectedDates[0];
     console.log(chosenDate);
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
+
     if (Date.now() > chosenDate) {
-      alert('Please choose a date in the future');
+      Notiflix.Notify.failure('Please choose a date in the future');
       refs.startBtn.disabled = true;
       return;
     }
@@ -25,4 +36,52 @@ const options = {
 };
 
 refs.startBtn.disabled = true;
+
 flatpickr('#datetime-picker', options);
+
+refs.startBtn.addEventListener('click', countdownTimer);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function countdownTimer() {
+  intervalId = setInterval(() => {
+    const currentDate = new Date();
+    const dateDifference = chosenDate - currentDate;
+
+    if (dateDifference <= 0) {
+      clearInterval(intervalId);
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = addLeadingZero(convertMs(dateDifference));
+    refs.dataDays.textContent = `${days}`;
+    refs.dataHours.textContent = `${hours}`;
+    refs.dataMinutes.textContent = `${minutes}`;
+    refs.dataSeconds.textContent = `${seconds}`;
+  }, 1000);
+}
+
+function addLeadingZero({ days, hours, minutes, seconds }) {
+  const daysNum = String(days).padStart(2, 0);
+  const hoursNum = String(hours).padStart(2, 0);
+  const minutesNum = String(minutes).padStart(2, 0);
+  const secondsNum = String(seconds).padStart(2, 0);
+  return { days: daysNum, hours: hoursNum, minutes: minutesNum, seconds: secondsNum };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,54 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+// Notiflix.Notify.success('Sol lucet omnibus');
+
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+// Notiflix.Notify.warning('Memento te hominem esse');
+
+// Notiflix.Notify.info('Cogito ergo sum');
+
+const startBtn = document.querySelector('.form');
+
+startBtn.addEventListener('submit', submitHandler);
+
+function submitHandler(e) {
+  e.preventDefault();
+
+  const inputRefs = e.currentTarget.elements;
+  const delayValue = inputRefs.delay.value;
+  const stepValue = inputRefs.step.value;
+  const amountValue = inputRefs.amount.value;
+
+  let totalDelay = Number(delayValue);
+
+  for (let i = 1; i <= Number(amountValue); i += 1) {
+    if (i === 1) {
+      createPromise(i, totalDelay).then(logSuccess).catch(logError);
+    } else {
+      totalDelay += Number(stepValue);
+      createPromise(i, totalDelay).then(logSuccess).catch(logError);
+    }
   }
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const logSuccess = ({ position, delay }) => {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+};
+
+const logError = ({ position, delay }) => {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+};
